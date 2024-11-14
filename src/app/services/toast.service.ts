5c1de65db2677f43931a2adec47bb60cd56a3e1f
Igor Brizack <igor_brizack@hotmail.com>
2024-11-14 19:20:44 -0300

    feat: spinner service

---
+++ src/app/services/toast.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  private toastSubject = new Subject<{ message: string; isError: boolean }>();
+  toastState$ = this.toastSubject.asObservable();
+
+  showError(message: string) {
+    this.toastSubject.next({ message, isError: true });
+  }
+}
