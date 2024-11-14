ae4eca7d88400e425b357c46efeec010997b7515
Igor Brizack <igor_brizack@hotmail.com>
2024-11-14 19:20:02 -0300

    feat: toast component

---
+++ src/app/components/toast/toast.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastService } from 'src/app/services/toast.service';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.css'],
+})
+export class ToastComponent implements OnInit {
+  message: string = '';
+  isError: boolean = false;
+  isVisible: boolean = false;
+
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit() {
+    this.toastService.toastState$.subscribe((toast) => {
+      console.log(toast);
+      this.message = toast.message;
+      this.isError = toast.isError;
+      this.showToast();
+    });
+  }
+
+  showToast() {
+    this.isVisible = true;
+    setTimeout(() => {
+      this.isVisible = false;
+    }, 3000);
+  }
+}
