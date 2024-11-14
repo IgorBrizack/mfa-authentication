63fcf13ecf0c6078aae5543a3622d50c5f5c1020
Igor Brizack <igor_brizack@hotmail.com>
2024-11-14 19:23:23 -0300

    feat: login component with iprovements

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/services/toast.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
+
+  email: string = '';
+  password: string = '';
 
   ngOnInit(): void {}
+
+  doLogin(email: string, password: string) {
+    console.log(email, password);
+    const response = this.userService.login(email, password).subscribe({
+      next: (data: any) => {
+        if (data && data.token) {
+          localStorage.setItem('core_token', data.token as any);
+        }
+
+        this.router.navigate(['/authentication']);
+      },
+      error: (err) => {
+        this.toastService.showError(err.error.error);
+      },
+    });
+  }
 }
