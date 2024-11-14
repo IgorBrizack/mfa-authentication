294a2b6795641a1b8c71b4008bd21f40a8d4bbee
Igor Brizack <igor_brizack@hotmail.com>
2024-11-14 19:20:27 -0300

    feat: spinner component

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.css']
+  styleUrls: ['./spinner.component.css'],
 })
 export class SpinnerComponent implements OnInit {
+  @Input() message: string = '';
+  @Input() isError: boolean = false;
+  isVisible: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.showToast();
   }
 
+  showToast() {
+    this.isVisible = true;
+    setTimeout(() => {
+      this.isVisible = false;
+    }, 3000);
+  }
 }
